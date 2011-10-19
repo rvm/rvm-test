53a5d0bcfe32123de5f7ddae54fdfd59a5208593
Deryl R. Doucette <me@deryldoucette.com>
2011-10-19 15:52:09 -0400

    Initial commit of rvm-test. This is what used to be under rvm_test_suite in the RVM Project. We're isolating it off to make it a separate entity within the RVM project as a whole.

---
+++ ruby/app/models/test_reports.rb
@@ -0,0 +1,17 @@
+class TestReport < ActiveRecord::Base
+  has_many :commands
+    
+  def record_timings(&cmds)
+    Benchmark.benchmark(CAPTION) do |x|
+      x.report("Timings: ", &cmds)
+    end
+  end
+
+  def run_command( cmd )
+    command = commands.build
+    command.run( cmd )
+    command.save
+    self.sysname = command.sysname
+  end
+  
+end
---
+++ ruby/db/migrate/20111015193007_command_migration.rb
@@ -0,0 +1,17 @@
+class CommandMigration < ActiveRecord::Migration
+  def self.up
+    create_table :commands do |t|
+      t.references :test_report
+      t.string :sysname     
+      t.string :os_type
+      t.string  :cmd
+      t.text  :cmd_output
+      t.string  :timings
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :commands
+  end
+end
