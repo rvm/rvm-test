8b1bab06c464e0fa01f2ac54c26cc876dcb0b33b
Deryl R. Doucette <me@deryldoucette.com>
2011-10-22 18:33:49 -0400

    Various updates and cullings.

--- ruby/app/models/test_reports.rb
+++ ruby/app/models/test_reports.rb
@@ -14,4 +14,22 @@ class TestReport < ActiveRecord::Base
     self.sysname = command.sysname
   end
   
+  def display_short_report
+    self.commands.each do |command|
+      puts "Test Report for : " + "#{command.sysname} - " + "Cmd ID: " + command.id.to_s + " - Executed: \"#{command.cmd.to_s}\"" + " at " +  "#{command.updated_at.to_s}"
+    end
+  end
+  
+  def display_long_report
+    # Next, we sort this particular report's commands on the ID field, then we can promise to display them in the order they were issued.
+    # We only want the commands associated with this particular TestReport object, so we use self to build the command.
+    self.commands.sort! { |old,cur| old.id <=> cur.id }
+    
+    self.commands.each do |command|
+      puts "\t\t\t\t*************** [ TESTING REPORT FOR #{command.sysname} ] ***************\t\t\t\t\n\n"
+      puts " REPORT ID #: #{self.id}\n COMMAND ID #: #{command.id}\n SYSTEM TYPE: #{command.os_type}\n EXECUTED COMMAND: #{command.cmd}\n TIMINGS: "
+      puts "#{command.timings} "
+      puts  "\nCOMMAND OUTPUT: #{command.cmd_output}\n"
+    end
+  end
 end
