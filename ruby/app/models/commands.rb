f95a81a75e49da822eaa7f09aa18a4f9e3b79b85
Deryl R. Doucette <me@deryldoucette.com>
2011-11-05 11:57:47 -0400

    Fixed commands. was outputting wrong var.

--- ruby/app/models/commands.rb
+++ ruby/app/models/commands.rb
@@ -1,5 +1,5 @@
 class Command < ActiveRecord::Base
-  belongs_to :test_reports
+  belongs_to :test_reports, :autosave => true
 
   before_create do |command|
     # Capture the system's name and its OS
@@ -33,10 +33,10 @@ class Command < ActiveRecord::Base
       self.exit_status = bash.status
       self.cmd_output = stdout.string
       self.error_msg = stderr.string
-      puts "command.run EXIT STATUS: #{Command.exit_status}"
+      puts "command.run EXIT STATUS: #{self.exit_status}"
       # Now, capture and display that we captured ENV from the shell for this command.
       self.env_closing = bash.execute "/usr/bin/printenv"
-      puts 'Captured closing environment - #{Command.env_closing}'
+      puts 'Captured closing environment - #{self.env_closing}'
       # Turn the Array of env strings into a Hash for later use - Thanks apeiros_
       self.env_closing = env_to_hash(self.env_closing[0])    
     end
