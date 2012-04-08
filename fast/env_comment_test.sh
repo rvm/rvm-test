d4a2a43fd12804c7033c5e40a76fefe5c5d2d77e
Michal Papis <mpapis@gmail.com>
2012-04-08 23:46:51 +0200

    update env test with --

--- fast/env_comment_test.sh
+++ fast/env_comment_test.sh
@@ -2,5 +2,6 @@ source "$rvm_path/scripts/rvm"
 
 rvm use 1.8.7-p358 --install
 
-rvm env 1.8.7-p358        # match=/1.8.7-p358/; match=/GEM_HOME=/; match=/GEM_PATH=/
-rvm env 1.8.7-p358 --path # match=/1.8.7-p358/; match=/environments/
+rvm env 1.8.7-p358           # match=/1.8.7-p358/; match=/GEM_HOME=/; match=/GEM_PATH=/
+rvm env 1.8.7-p358 --path    # match=/1.8.7-p358/; match=/environments/
+rvm env 1.8.7-p358 -- --path # match=/1.8.7-p358/; match=/environments/
