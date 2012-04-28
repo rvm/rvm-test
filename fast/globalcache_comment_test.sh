288b6e451396dc8c997d7d2b4cdbfc6afd823fee
Michal Papis <mpapis@gmail.com>
2012-04-28 19:26:12 +0200

    improve glonbalcache test

--- fast/globalcache_comment_test.sh
+++ fast/globalcache_comment_test.sh
@@ -1,12 +1,13 @@
 source "$rvm_path/scripts/rvm"
 
 rvm use 1.9.3 --install                           # status=0
-
-grep globalcache "$rvm_path/user/db"              # status=1
+rvm --force gemset globalcache disable
+rvm gemset globalcache enabled                    # match=/Disabled/
 
 [[ -L "$rvm_path/gems/cache" ]]                   # status=1
 
 rvm gemset globalcache enable                     # status=0; match=/ global cache /
+rvm gemset globalcache enabled                    # match=/Enabled/
 rvm gemset list                                   # status=0; match!=/ testset /
 
 rvm 1.9.3 do rvm gemset create testset            # status=0; match=/ gemset created /
@@ -19,7 +20,5 @@ rvm system                                        # status=0
 [[ -L "$rvm_path/gems/cache" ]]                   # status=1
 [[ -d "$rvm_path/gems/cache" ]]                   # status=0
 
-[[ -L "$rvm_path/gems/cache" ]] && rm -f "$rvm_path/gems/cache"
-
 rvm --force gemset globalcache disable            # status=0; match=/Removing the global cache/
-
+rvm gemset globalcache enabled                    # match=/Disabled/
