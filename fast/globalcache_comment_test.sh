3bb6f9e6920d1d1951093c165396efe37cb8fea5
Michal Papis <mpapis@gmail.com>
2012-05-13 07:30:46 +0200

    add checking status for globalcache, fix rvm#965

--- fast/globalcache_comment_test.sh
+++ fast/globalcache_comment_test.sh
@@ -4,21 +4,22 @@ rvm use 1.9.3 --install                           # status=0
 rvm --force gemset globalcache disable
 rvm gemset globalcache enabled                    # match=/Disabled/
 
-[[ -L "$rvm_path/gems/cache" ]]                   # status=1
+[[ -L "$rvm_path/gems/cache" ]]                   # status!=0
 
 rvm gemset globalcache enable                     # status=0; match=/ global cache /
-rvm gemset globalcache enabled                    # match=/Enabled/
+rvm gemset globalcache enabled                    # status=0; match=/Enabled/
 rvm gemset list                                   # status=0; match!=/ testset /
 
 rvm 1.9.3 do rvm gemset create testset            # status=0; match=/ gemset created /
 
-[[ -L "$rvm_path/gems/cache" ]]                   # status=1
+[[ -L "$rvm_path/gems/cache" ]]                   # status!=0
 [[ -d "$rvm_path/gems/cache" ]]                   # status=0
 
 rvm system                                        # status=0
 
-[[ -L "$rvm_path/gems/cache" ]]                   # status=1
+[[ -L "$rvm_path/gems/cache" ]]                   # status!=0
 [[ -d "$rvm_path/gems/cache" ]]                   # status=0
+[[ -d "$rvm_path/gems/cache/cache" ]]             ## status!=0 ... need more testing for this
 
-rvm --force gemset globalcache disable            # status=0; match=/Removing the global cache/
-rvm gemset globalcache enabled                    # match=/Disabled/
+rvm --force gemset globalcache disable            # status=0;  match=/Removing the global cache/
+rvm gemset globalcache enabled                    # status!=0; match=/Disabled/
