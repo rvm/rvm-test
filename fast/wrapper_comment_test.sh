f5173d11f4061b44d57ee4d0f6c197bf314af4b8
Michal Papis <mpapis@gmail.com>
2012-05-14 07:22:55 +0200

    improve wrapper tests, update #12

--- fast/wrapper_comment_test.sh
+++ fast/wrapper_comment_test.sh
@@ -7,17 +7,16 @@ ivnp=$iv-p999    ## installed ruby version but invalid patchlevel
 nvp=$nv-p999     ## non-existent ruby version and patchlevel
 
 wdir="$rvm_path/wrappers/ruby-$ivp"
+bdir="$rvm_path/bin"
 
-rvm install $ivp
+rvm install $ivp                     # status=0
 
-: insufficient parameters
-rvm wrapper                          # status!=0; match=/Not enough parameters/
-rvm wrapper $nv                      # status!=0; match=/Not enough parameters/
-rvm wrapper $nvp                     # status!=0; match=/Not enough parameters/
-rvm wrapper $iv                      # status!=0; match=/Not enough parameters/
-rvm wrapper $ivp                     # status!=0; match=/Not enough parameters/
+: help
+rvm wrapper                          # status!=0; match=/Usage:/
 
 : non-existent version
+rvm wrapper $nv                      # status!=0; match=/Could not load ruby .*1\.2\.3/
+rvm wrapper $nvp                     # status!=0; match=/Could not load ruby .*1\.2\.3/
 rvm wrapper $nv --no-prefix          # status!=0; match=/Could not load ruby .*1\.2\.3/
 rvm wrapper $nv --no-prefix erb      # status!=0; match=/Could not load ruby .*1\.2\.3/
 rvm wrapper $nv myprefix             # status!=0; match=/Could not load ruby .*1\.2\.3/
@@ -36,49 +35,53 @@ rvm wrapper $ivnp myprefix           # status!=0; match=/Could not load ruby .*1
 rvm wrapper $ivnp myprefix erb       # status!=0; match=/Could not load ruby .*1\.9\.1-p999/
 
 : installed version
+rvm wrapper $iv                      # status=0;  match=/\A\Z/
+[[ -L $bdir/erb-ruby-$ivp ]]         # status=0
+rm -f $wdir/{erb,gem,irb,rake,rdoc,ri,ruby,testrb}      # status=0
+rm -f $bdir/{erb,gem,irb,rake,rdoc,ri,testrb}-ruby-$ivp # status=0
+rm -f $bdir/ruby-$ivp                # status=0
+
 rvm wrapper $iv --no-prefix          # status=0;  match=/\A\Z/
-rm $wdir/{erb,gem,irb,rake,rdoc,ri,ruby,testrb} 
-                                     # status=0
+[[ -L $bdir/erb ]]                   # status=0
+rm -f $wdir/{erb,gem,irb,rake,rdoc,ri,ruby,testrb} # status=0
+rm -f $bdir/{erb,gem,irb,rake,rdoc,ri,ruby,testrb} # status=0
+
 rvm wrapper $iv myprefix             # status=0;  match=/\A\Z/
-rm $wdir/myprefix-{erb,gem,irb,rake,rdoc,ri,ruby,testrb} 
-                                     # status=0
+[[ -L $bdir/myprefix_erb ]]          # status=0
+rm -f $wdir/{erb,gem,irb,rake,rdoc,ri,ruby,testrb}          # status=0
+rm -f $bdir/myprefix_{erb,gem,irb,rake,rdoc,ri,ruby,testrb} # status=0
 
 : installed version and patchlevel
 rvm wrapper $ivp --no-prefix         # status=0;  match=/\A\Z/
-rm $wdir/{erb,gem,irb,rake,rdoc,ri,ruby,testrb} 
-                                     # status=0
-rvm wrapper $ivp myprefix            # status=0;  match=/\A\Z/
-rm $wdir/myprefix-{erb,gem,irb,rake,rdoc,ri,ruby,testrb} 
-                                     # status=0
+[[ -L $bdir/erb ]]                   # status=0
+rm -f $wdir/{erb,gem,irb,rake,rdoc,ri,ruby,testrb} # status=0
+rm -f $bdir/{erb,gem,irb,rake,rdoc,ri,ruby,testrb} # status=0
+
+rvm wrapper $ivp myprefix            # status=0
+[[ -L $bdir/myprefix_erb ]]          # status=0
+rm -f $wdir/{erb,gem,irb,rake,rdoc,ri,ruby,testrb}          # status=0
+rm -f $bdir/myprefix_{erb,gem,irb,rake,rdoc,ri,ruby,testrb} # status=0
 
 : installed version, single binary
 rvm wrapper $iv --no-prefix erb      # status=0;  match=/\A\Z/
-rm $wdir/erb                         # status=0
-[[ -e $wdir/rake ]]                  # status!=0
-rvm wrapper $iv myprefix erb         # status=0;  match=/\A\Z/
-rm $wdir/myprefix-erb                # status=0
-[[ -e $wdir/myprefix-rake ]]         # status!=0
+[[ -L $bdir/erb ]]                   # status=0
+rm -f $wdir/erb                      # status=0
+rm -f $bdir/erb                      # status=0
 
-: installed version and patchlevel, single binary
-rvm wrapper $ivp --no-prefix erb     # status=0;  match=/\A\Z/
-rm $wdir/erb                         # status=0
-[[ -e $wdir/rake ]]                  # status!=0
-rvm wrapper $ivp myprefix erb        # status=0;  match=/\A\Z/
-rm $wdir/myprefix-erb                # status=0
-[[ -e $wdir/myprefix-rake ]]         # status!=0
+rvm wrapper $iv myprefix erb         # status=0;  match=/\A\Z/
+[[ -L $bdir/myprefix_erb ]]          # status=0
+rm -f $wdir/erb                      # status=0
+rm -f $bdir/myprefix_erb             # status=0
 
 : installed version, multiple binaries
 rvm wrapper $iv --no-prefix erb irb  # status=0;  match=/\A\Z/
-rm $wdir/{erb,irb}                   # status=0
-[[ -e $wdir/rake ]]                  # status!=0
-rvm wrapper $iv myprefix erb irb     # status=0;  match=/\A\Z/
-rm $wdir/myprefix-{erb,irb}          # status=0
-[[ -e $wdir/myprefix-rake ]]         # status!=0
+[[ -L $bdir/erb ]]                   # status=0
+[[ -L $bdir/irb ]]                   # status=0
+rm -f $wdir/{erb,irb}                # status=0
+rm -f $bdir/{erb,irb}                # status=0
 
-: installed version and patchlevel, multiple binaries
-rvm wrapper $ivp --no-prefix erb irb # status=0;  match=/\A\Z/
-rm $wdir/{erb,irb}                   # status=0
-[[ -e $wdir/rake ]]                  # status!=0
-rvm wrapper $ivp myprefix erb irb    # status=0;  match=/\A\Z/
-rm $wdir/myprefix-{erb,irb}          # status=0
-[[ -e $wdir/myprefix-rake ]]         # status!=0
+rvm wrapper $iv myprefix erb irb     # status=0;  match=/\A\Z/
+[[ -L $bdir/myprefix_erb ]]          # status=0
+[[ -L $bdir/myprefix_irb ]]          # status=0
+rm -f $wdir/{erb,irb}                # status=0
+rm -f $bdir/myprefix_{erb,irb}       # status=0
