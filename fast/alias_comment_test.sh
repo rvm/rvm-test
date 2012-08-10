1e841d0a46f558123e91319840cae336ad8a81b2
Michal Papis <mpapis@gmail.com>
2012-08-10 06:12:34 +0200

    new rules for aliases for 1.15.0

--- fast/alias_comment_test.sh
+++ fast/alias_comment_test.sh
@@ -10,8 +10,20 @@ rvm alias delete default                   # status=0
 ls -l $rvm_path/environments/default       # status!=0
 
 rvm alias create ruby-test-default 1.9.2@test_gemset # status=0
-ls -l $rvm_path/environments/ruby-test-default       # status!=0
+ls -l $rvm_path/environments/ruby-test-default       # status=0
 rvm alias list                                       # match=/^ruby-test-default => ruby-1.9.2.*@test_gemset$/
 rvm alias delete ruby-test-default                   # status=0
+ls -l $rvm_path/environments/ruby-test-default       # status!=0
 
 rvm --force gemset delete test_gemset
+
+: overwrite existing aliases
+rvm alias create veve 1.9.3-p194  # status=0
+ls -l $rvm_path/environments/veve # status=0
+rvm alias list                    # match=/^veve => ruby-1.9.3-p194$/
+rvm alias create veve 1.9.2       # status=0
+ls -l $rvm_path/environments/veve # status=0
+rvm alias list                    # match=/^veve => ruby-1.9.2-p.*$/
+rvm alias delete veve             # status=0
+ls -l $rvm_path/environments/veve # status!=0
+rvm alias list                    # match!=/^veve => /
