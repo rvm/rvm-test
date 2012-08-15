3c3651dfcadc10f5f417d23b9a487c6398fd0316
Michal Papis <mpapis@gmail.com>
2012-08-15 05:41:49 +0200

    add test for cleaning gemset name in rvm do

--- fast/do_comment_test.sh
+++ fast/do_comment_test.sh
@@ -12,5 +12,7 @@ rvm 1.8.7@test0 do rvm gemdir # status=1; match=/is not installed/
 rvm 1.8.7@test1 do rvm gemdir # status=0; match=/1.8.7-p[[:digit:]]+@test1/
 rvm 1.8.7@test2 do rvm gemdir # status=0; match=/1.8.7-p[[:digit:]]+@test2/
 
+rvm 1.8.7@global,1.8.7 do rvm gemdir # status=0; match=/1.8.7-p[[:digit:]]+@global$/; match=/1.8.7-p[[:digit:]]+$/
+
 rvm --force gemset delete test1 # status=0
 rvm --force gemset delete test2 # status=0
