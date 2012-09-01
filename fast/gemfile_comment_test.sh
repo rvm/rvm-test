72f675fe137711e30bcbf1895c57284c64f7937c
Michal Papis <mpapis@gmail.com>
2012-09-01 22:05:51 +0200

    extended test for wayneeseguin/rvm#911

--- fast/gemfile_comment_test.sh
+++ fast/gemfile_comment_test.sh
@@ -8,23 +8,40 @@ f_gemfile=$d/sub/Gemfile
 
 : prepare1
 rvm use 1.9.3 --install
+rvm gemset create gemfile
+rvm gemset create rvmrc
 mkdir -p $d/sub
-printf "rvm use 1.9.3@rvmrc --create\n"   > $f_rvmrc
+printf "rvm use 1.9.3@rvmrc\n"   > $f_rvmrc
 
 : test1
-rvm current           # match!=/^ruby-1.9.3-p[[:digit:]]+@rvmrc$/
+rvm current           # match=/^ruby-1.9.3-p[[:digit:]]+$/
+rvm_current_rvmrc=""
 rvm rvmrc load $d/sub # status=0; match=/Using .*ruby-1.9.3-p[[:digit:]]+.*rvmrc/
 rvm current           # match=/^ruby-1.9.3-p[[:digit:]]+@rvmrc$/
 
 : prepare2
-rvm use 1.9.3 --install
+rvm use 1.9.3
 printf "source :rubygems\n\ngem 'haml'\n" > $f_gemfile
 
 : test2
-rvm current           # match!=/^ruby-1.9.3-p[[:digit:]]+@rvmrc$/
+rvm current           # match=/^ruby-1.9.3-p[[:digit:]]+$/
+rvm_current_rvmrc=""
 rvm rvmrc load $d/sub # status=0; match=/Using .*ruby-1.9.3-p[[:digit:]]+.*rvmrc/
 rvm current           # match=/^ruby-1.9.3-p[[:digit:]]+@rvmrc$/
 
+: prepare3
+rvm use 1.9.3
+## escape # -> \x23
+printf "source :rubygems\n\x23ruby=1.9.3\n\x23ruby-gemset=gemfile\ngem 'haml'\n" > $f_gemfile
+
+: test3
+rvm current           # match=/^ruby-1.9.3-p[[:digit:]]+$/
+rvm_current_rvmrc=""
+rvm rvmrc load $d/sub # status=0; match!=/Using .*ruby-1.9.3-p[[:digit:]]+.*rvmrc/
+rvm current           # match=/^ruby-1.9.3-p[[:digit:]]+@gemfile$/
+
 : clean
-rvm 1.9.3 do rvm --force gemset delete rvmrc
+rvm use 1.9.3
+rvm --force gemset delete gemfile
+rvm --force gemset delete rvmrc
 rm -rf $d
