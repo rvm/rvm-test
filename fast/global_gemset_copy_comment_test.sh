b84bdf679ae9e97ce5bf38fbfbf8ca8cfd07fc22
David Deryl Downey <me@daviddwdowney.com>
2012-10-20 17:57:09 -0400

    Added fast test for global-to-global 'rvm gemset copy' scneario. This was failing earlier. This test verifies that it now succeeds. In addition, this test covers the failing to overwrite the existing cache directory. This test does currently fail on 2 tests which are really 2-in-1 but covering both aspects of the existing cache directory issue. That is: match!=/cannot overwrite directory/; match!=/with non-directory/;

---
+++ fast/global_gemset_copy_comment_test.sh
@@ -0,0 +1,15 @@
+rvm use 1.9.3-p286 --install # status=0
+rvm use 1.9.3-p194 --install # status=0
+rvm get head # status=0 ; match=/Downloading RVM from wayneeseguin branch master/; match=/RVM reloaded/
+rvm --force gemset globalcache disable
+rvm gemset globalcache enabled # status=1; match=/ Disabled/
+rvm gemset globalcache enable # status=0; match=/ global cache /
+rvm gemset globalcache enabled # status=0; match=/ Enabled/
+rvm gemset list # status=0; match=/ global$/
+rvm gemset export 1.9.3-p194@global  193-p194-global.gems # status=0; match=/Exporting current environments gemset/
+rvm gemset export 1.9.3-p286@global  193-p286-global.gems # status=0; match=/Exporting current environments gemset/
+rvm gemset empty 1.9.3-p286@global  --force # status=0
+rvm --trace gemset copy 1.9.3-p194@global 1.9.3-p286@global # status=0; match=/Copying gemset/; match!=/Unknown file type/; match!=/cannot overwrite directory/; match!=/with non-directory/; match!=/Error running/
+rvm 1.9.3-p286@global do bash -c 'GEM_PATH=$GEM_DIR gem list' # status=0; match=/rvm/
+rvm gemset --force empty 1.9.3-p286@global # status=0
+rvm get master # status=0 ; match=/RVM reloaded/
