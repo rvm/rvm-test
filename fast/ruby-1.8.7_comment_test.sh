087a32423d87debd3d9754ddc1f68c7a87a8b50c
Michal Papis <mpapis@gmail.com>
2013-02-16 12:42:11 +0100

    add testing if all files have been removed after remove

--- fast/ruby-1.8.7_comment_test.sh
+++ fast/ruby-1.8.7_comment_test.sh
@@ -1,16 +1,35 @@
 source "$rvm_path/scripts/rvm"
 
-rvm reinstall 1.8.7-ntest --skip-gemsets
+rvm remove  1.8.7-ntest --gems
+rvm install 1.8.7-ntest --skip-gemsets -j $(cat /proc/cpuinfo 2>/dev/null | (grep vendor_id || echo 'vendor_id : Other';) | wc -l)
 # status=0
 # match!=/Already installed/
 # match=/Applying patch/
 # match=/Skipped importing default gemsets/
 # match=/patches just to be compiled on/
+
+rvm install 1.8.7-ntest
+# status=0; match=/Already installed/
+
+rvm 1.8.7-ntest do which gem
+# match=/1.8.7-p[[:digit:]]+-ntest/
+
+rvm 1.8.7-ntest do gem env
+
 rvm 1.8.7-ntest do gem list
-# match!=/bundler/
-# match!=/rake/
-# match!=/rubygems-bundler/
-# match!=/rvm/
-rvm 1.8.7-ntest do ruby -v    # match=/1.8.7/
-rvm install 1.8.7-ntest       # status=0; match=/Already installed/
-rvm remove 1.8.7-ntest --gems # status=0; match=/Removing/
+# match[stderr]=/\A\Z/
+# match[stdout]!=/bundler/
+# match[stdout]!=/rake/
+# match[stdout]!=/rubygems-bundler/
+# match[stdout]!=/rvm/
+
+rvm 1.8.7-ntest do ruby -v
+# match=/1.8.7/
+
+rvm remove 1.8.7-ntest --gems
+# status=0; match=/Removing/
+
+find -L $rvm_path/environments -regextype posix-extended -regex '.*1.8.7-p[[:digit:]]+-ntest.*' -print -quit # match=/^$/
+find -L $rvm_path/wrappers     -regextype posix-extended -regex '.*1.8.7-p[[:digit:]]+-ntest.*' -print -quit # match=/^$/
+find -L $rvm_path/gems         -regextype posix-extended -regex '.*1.8.7-p[[:digit:]]+-ntest.*' -print -quit # match=/^$/
+find -L $rvm_path/bin          -regextype posix-extended -regex '.*1.8.7-p[[:digit:]]+-ntest.*' -print -quit # match=/^$/
