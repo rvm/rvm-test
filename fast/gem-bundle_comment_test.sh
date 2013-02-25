4eb2a304e65a03226b0f65ad1172683a77e8539d
Michal Papis <mpapis@gmail.com>
2013-02-25 03:14:40 +0100

    switch to bundler --pre to match latest ruby 2.0.0 requirements

--- fast/gem-bundle_comment_test.sh
+++ fast/gem-bundle_comment_test.sh
@@ -10,9 +10,9 @@ rvm gemset use gemtest # status=0
 
 : test
 ## this only happens after installing rvm: match=/Gem bundler is not installed/
-bundle config       # status=127
-gem install bundler # status=0
-bundle config       # status=0 ; match=/Settings are listed in order of priority/
+bundle config             # status=127
+gem install bundler --pre # status=0
+bundle config             # status=0 ; match=/Settings are listed in order of priority/
 
 : reset
 rvm gemset use default
