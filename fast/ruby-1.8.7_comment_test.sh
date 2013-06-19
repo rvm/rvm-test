afb75ea66f53e16417db30e7866d4937679246a7
Michal Papis <mpapis@gmail.com>
2013-06-19 17:53:17 +0200

    add a test for defalt/global gemsets

--- fast/ruby-1.8.7_comment_test.sh
+++ fast/ruby-1.8.7_comment_test.sh
@@ -1,6 +1,9 @@
 source "$rvm_path/scripts/rvm"
 
 rvm remove  1.8.7-ntest --gems
+
+## without gemsets
+
 rvm install 1.8.7-ntest --skip-gemsets
 # status=0
 # match!=/Already installed/
@@ -26,8 +29,39 @@ rvm 1.8.7-ntest do gem list
 rvm 1.8.7-ntest do ruby -v
 # match=/1.8.7/
 
+rvm remove  1.8.7-ntest --gems
+# status=0; match=/Removing/
+
+
+## default/global gemsets
+
+mkdir -p $rvm_path/gemsets/ruby/1.8.7/
+echo haml > $rvm_path/gemsets/ruby/1.8.7/default.gems
+echo tf > $rvm_path/gemsets/ruby/1.8.7/global.gems
+
+rvm install 1.8.7-ntest
+# status=0
+# match!=/Already installed/
+# match=/Applying patch/
+# match=/importing default gemsets, this may take time/
+# match=/patches just to be compiled on/
+
+rvm 1.8.7-ntest do gem list
+# match[stderr]=/\A\Z/
+# match[stdout]=/haml/
+# match[stdout]=/tf/
+
+rvm 1.8.7-ntest@global do gem list
+# match[stderr]=/\A\Z/
+# match[stdout]!=/haml/
+# match[stdout]=/tf/
+
+## Cleanup
+
 rvm remove 1.8.7-ntest --gems
 # status=0; match=/Removing/
+
+rm -rf $rvm_path/gemsets/ruby/1.8.7/
 
 find -L $rvm_path/environments -regextype posix-extended -regex '.*1.8.7-p[[:digit:]]+-ntest.*' -print -quit # match=/^$/
 find -L $rvm_path/wrappers     -regextype posix-extended -regex '.*1.8.7-p[[:digit:]]+-ntest.*' -print -quit # match=/^$/
