3c6633930047a470ad64e5ba568e6bd387f07e0b
Michal Papis <mpapis@gmail.com>
2013-07-31 00:08:00 +0200

    test .ruby-version

---
+++ fast/ruby-version_comment_test.sh
@@ -0,0 +1,41 @@
+source "$rvm_path/scripts/rvm"
+
+: prepare
+true TMPDIR:${TMPDIR:=/tmp}:
+d=$TMPDIR/test-ruby-version
+f=$d/.ruby-version
+g=$d/.ruby-gemset
+mkdir -p $d
+cd $d
+rvm use --install 1.9.3-p392
+rvm use --install 1.9.3
+rvm use --install 1.8.7
+
+## simple
+: short version
+echo "1.9.3" > $f           # env[GEM_HOME]=/1.8.7/
+rvm use .                   # env[GEM_HOME]=/1.9.3/
+
+: ruby version
+rvm use 1.8.7
+echo "ruby-1.9.3" > $f      # env[GEM_HOME]=/1.8.7/
+rvm use .                   # env[GEM_HOME]=/1.9.3/
+
+: patch version
+rvm use 1.8.7
+echo "1.9.3-p392" > $f      # env[GEM_HOME]=/1.8.7/
+rvm use .                   # env[GEM_HOME]=/1.9.3-p392/
+
+: full version
+rvm use 1.8.7
+echo "ruby-1.9.3-p392" > $f # env[GEM_HOME]=/1.8.7/
+rvm use .                   # env[GEM_HOME]=/1.9.3-p392/
+
+: gemset
+rvm use 1.8.7
+echo "veve" > $g            # env[GEM_HOME]=/1.8.7/
+rvm use .                   # env[GEM_HOME]=/1.9.3-p392@veve/
+
+: clean
+cd ..
+rm -rf $d
