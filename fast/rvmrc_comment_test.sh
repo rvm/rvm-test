547fa269eecafbd83559d88ac000bfd73c457292
Michal Papis <mpapis@gmail.com>
2013-08-08 06:08:33 +0200

    add test for https://github.com/wayneeseguin/rvm/issues/2078

--- fast/rvmrc_comment_test.sh
+++ fast/rvmrc_comment_test.sh
@@ -7,6 +7,8 @@ d=$TMPDIR/test-rvmrc
 f=$d/.rvmrc
 mkdir -p $d
 echo "echo loading-rvmrc" > $f
+rvm use 1.9.3-p392 --install # status=0
+rvm use 1.9.3-p448 --install # status=0
 
 ## simple
 : trust
@@ -37,13 +39,37 @@ rvm rvmrc trust "$ds"     # match=/ as trusted$/
 rvm rvmrc trusted "$ds"   # match=/is currently trusted/
 rvm rvmrc reset "$ds"     # match=/^Reset/
 
-## load
+: prepare for load/cd test
+rvm alias create default 1.9.3-p448
+rvm alias list            # match=/default => ruby-1.9.3-p448/
 export rvm_project_rvmrc_default=1
+
+## load default
 builtin cd
-rvm use 1.9.3-p448 --default --install # status=0
-rvm alias list            # match=/default/
+rvm use 1.9.3-p392
 rvm rvmrc load            # env[GEM_HOME]!=/^$/
 
+## load ruby
+cd
+rvm use 1.9.3-p392
+echo "rvm use 1.9.3-p448" > "$d/.rvmrc"
+rvm rvmrc trust "$d"      # match=/ as trusted$/
+cd "$d"                   # env[GEM_HOME]=/1.9.3-p448$/
+
+## load ruby@gemset
+cd
+rvm use 1.9.3-p392
+echo "rvm use 1.9.3-p448@vee --create" > "$d/.rvmrc"
+rvm rvmrc trust "$d"      # match=/ as trusted$/
+cd "$d"                   # env[GEM_HOME]=/1.9.3-p448@vee$/
+
+## load @gemset
+cd
+rvm use 1.9.3-p392
+echo "rvm use @vee --create" > "$d/.rvmrc"
+rvm rvmrc trust "$d"      # match=/ as trusted$/
+cd "$d"                   # env[GEM_HOME]=/1.9.3-p448@vee$/
+
 : clean
-rvm alias delete default
+rvm alias delete default  # status=0
 rm -rf $d
