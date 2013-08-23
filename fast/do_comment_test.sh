0a271d0974e3737cc442f24831a235680aec3b4c
Adam Spiers <tf@adamspiers.org>
2013-08-23 10:15:49 +0100

    test "rvm in ... do ..."
    This adds tests for https://github.com/wayneeseguin/rvm/pull/2152
    but also for many "rvm in ... do ..." cases which should succeed.

--- fast/do_comment_test.sh
+++ fast/do_comment_test.sh
@@ -16,3 +16,62 @@ rvm 1.8.7@global,1.8.7 do rvm gemdir # status=0; match=/1.8.7-p[[:digit:]]+@glob
 
 rvm --force gemset delete test1 # status=0
 rvm --force gemset delete test2 # status=0
+
+: FIXME: The following tests have awful duplication due to https://github.com/mpapis/tf/issues/6
+
+: -----------------------------------------------------------------
+: do in directory with no version
+
+true TMPDIR:${TMPDIR:=/tmp}:
+d=$TMPDIR/test-rvm-do-in
+
+: absolute directory
+rvm in $d do rvm info ruby # status=1; match=/Could not determine which Ruby to use/
+rvm in $d do ruby --version # status=1; match=/Could not determine which Ruby to use/
+
+: relative directory
+cd $TMPDIR
+rvm in test-rvm-do-in do rvm info ruby # status=1; match=/Could not determine which Ruby to use/
+rvm in test-rvm-do-in do ruby --version # status=1; match=/Could not determine which Ruby to use/
+
+: current directory
+cd $d
+rvm . do rvm info ruby # status=1; match=/Could not determine which Ruby to use/
+rvm . do ruby --version # status=1; match=/Could not determine which Ruby to use/
+
+: -----------------------------------------------------------------
+mkdir -p $d/1.8.7
+echo "echo 1.8.7" > $d/1.8.7/.ruby-version
+
+: absolute directory
+rvm in $d/1.8.7 do rvm info ruby # status=0; match=/version: *"1.8.7/
+rvm in $d/1.8.7 do ruby --version # status=0; match=/^ruby 1.8.7 /
+
+: relative directory
+cd $d
+rvm in 1.8.7 do rvm info ruby # status=0; match=/version: *"1.8.7/
+rvm in 1.8.7 do ruby --version # status=0; match=/^ruby 1.8.7 /
+
+: current directory
+cd $d/1.8.7
+rvm . do rvm info ruby # status=0; match=/version: *"1.8.7/
+rvm . do ruby --version # status=0; match=/^ruby 1.8.7 /
+
+: -----------------------------------------------------------------
+ver=1.9.3
+mkdir -p $d/1.9.3
+echo "echo 1.9.3" > $d/1.9.3/.ruby-version
+
+: absolute directory
+rvm in $d/1.9.3 do rvm info ruby # status=0; match=/version: *"1.9.3/
+rvm in $d/1.9.3 do ruby --version # status=0; match=/^ruby 1.9.3 /
+
+: relative directory
+cd $d
+rvm in 1.9.3 do rvm info ruby # status=0; match=/version: *"1.9.3/
+rvm in 1.9.3 do ruby --version # status=0; match=/^ruby 1.9.3 /
+
+: current directory
+cd $d/1.9.3
+rvm . do rvm info ruby # status=0; match=/version: *"1.9.3/
+rvm . do ruby --version # status=0; match=/^ruby 1.9.3 /
