1909dcbda12bdaf920833fc89019b26166e2727b
Michal Papis <mpapis@gmail.com>
2013-10-30 12:16:20 +0100

    add test for ignoring Gemfile warning after current path was ignored, update wayneeseguin/rvm#2328

--- fast/rvmrc-warning_comment_test.sh
+++ fast/rvmrc-warning_comment_test.sh
@@ -30,6 +30,12 @@ rvm rvmrc warning check  $f          # status=1 ; match=/is not ignored/
 rvm rvmrc warning check  allGemfiles # status=1 ; match=/is not ignored/
 rvm rvmrc warning list               # match!=/Gemfile/
 
+: single + all
+rvm rvmrc warning ignore $f          # status=0
+rvm rvmrc warning ignore allGemfiles # status=0
+rvm rvmrc warning list               # match=/allGemfiles/ ; match=/test-rvmrc-warning/Gemfile/
+rvm rvmrc warning check  $f          # status=0 ; match=/is ignored/
+rvm rvmrc warning check  allGemfiles # status=0 ; match=/is ignored/
 
 : clean
 mv -f $d/rvmrc_ignored $rvm_path/user/rvmrc_ignored
