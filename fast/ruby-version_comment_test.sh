b2d3841be43dcbd4085626da6d0d80993990d90c
Michal Papis <mpapis@gmail.com>
2014-03-26 00:55:49 +0100

    speed up test by not using older rubies

--- fast/ruby-version_comment_test.sh
+++ fast/ruby-version_comment_test.sh
@@ -7,7 +7,7 @@ f=$d/.ruby-version
 g=$d/.ruby-gemset
 mkdir -p $d
 cd $d
-rvm use --install 1.9.3-p392
+rvm use --install 1.9.3-p484
 rvm use --install 1.9.3
 rvm use --install 1.8.7
 
@@ -23,18 +23,18 @@ rvm use .                   # env[GEM_HOME]=/1.9.3/
 
 : patch version
 rvm use 1.8.7
-echo "1.9.3-p392" > $f      # env[GEM_HOME]=/1.8.7/
-rvm use .                   # env[GEM_HOME]=/1.9.3-p392/
+echo "1.9.3-p484" > $f      # env[GEM_HOME]=/1.8.7/
+rvm use .                   # env[GEM_HOME]=/1.9.3-p484/
 
 : full version
 rvm use 1.8.7
-echo "ruby-1.9.3-p392" > $f # env[GEM_HOME]=/1.8.7/
-rvm use .                   # env[GEM_HOME]=/1.9.3-p392/
+echo "ruby-1.9.3-p484" > $f # env[GEM_HOME]=/1.8.7/
+rvm use .                   # env[GEM_HOME]=/1.9.3-p484/
 
 : gemset
 rvm use 1.8.7
 echo "veve" > $g            # env[GEM_HOME]=/1.8.7/
-rvm use .                   # env[GEM_HOME]=/1.9.3-p392@veve/
+rvm use .                   # env[GEM_HOME]=/1.9.3-p484@veve/
 
 : clean
 cd ..
--- fast/rvm-prompt_comment_test.sh
+++ fast/rvm-prompt_comment_test.sh
@@ -1,15 +1,15 @@
 source "$rvm_path/scripts/rvm"
 
 command rvm install 1.8.7
-command rvm install 1.9.2
 command rvm install 1.9.3
+command rvm install 2.0.0
 
 rvm 1.9.1 do rvm-prompt      # match=/Ruby (ruby-)?1.9.1(-p[[:digit:]]+)? is not installed./
-rvm 1.9.2 do rvm-prompt      # match=/^ruby-1.9.2-p[[:digit:]]+$/
-rvm 1.9.3 do rvm-prompt i    # match=/^ruby$/
-rvm 1.9.3 do rvm-prompt i v  # match=/^ruby-1.9.3$/
-rvm 1.9.3 do rvm-prompt v    # match=/^1.9.3$/
-rvm 1.9.3 do rvm-prompt v p  # match=/^1.9.3-p[[:digit:]]+$/
+rvm 1.9.3 do rvm-prompt      # match=/^ruby-1.9.3-p[[:digit:]]+$/
+rvm 2.0.0 do rvm-prompt i    # match=/^ruby$/
+rvm 2.0.0 do rvm-prompt i v  # match=/^ruby-2.0.0$/
+rvm 2.0.0 do rvm-prompt v    # match=/^2.0.0$/
+rvm 2.0.0 do rvm-prompt v p  # match=/^2.0.0-p[[:digit:]]+$/
 rvm system do rvm-prompt     # match=/^$/
 rvm system do rvm-prompt s v # match=/^system$/
 rvm 1.8.7 do rvm-prompt s v  # match=/^1.8.7$/
