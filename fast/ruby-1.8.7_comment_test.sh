b0ac605c3c42993da12fcf0a20f0bdb3f5ae4ecd
Michal Papis <mpapis@gmail.com>
2014-03-26 02:17:01 +0100

    do not use regexps in 1.8.7-ntest

--- fast/ruby-1.8.7_comment_test.sh
+++ fast/ruby-1.8.7_comment_test.sh
@@ -1,10 +1,10 @@
 source "$rvm_path/scripts/rvm"
 
-rvm remove  1.8.7-ntest --gems
+rvm remove  1.8.7-p374-ntest --gems
 
 ## without gemsets
 
-rvm install 1.8.7-ntest --skip-gemsets
+rvm install 1.8.7-p374-ntest --skip-gemsets
 # status=0
 # match!=/Already installed/
 # match=/[Aa]pplying patch/
@@ -12,25 +12,25 @@ rvm install 1.8.7-ntest --skip-gemsets
 # match=/WARNING: Please be aware that you just installed a ruby that/
 # match=/for a list of maintained rubies visit:/
 
-rvm install 1.8.7-ntest
+rvm install 1.8.7-p374-ntest
 # status=0; match=/Already installed/
 
-rvm 1.8.7-ntest do which gem
+rvm 1.8.7-p374-ntest do which gem
 # match=/1.8.7-p[[:digit:]]+-ntest/
 
-rvm 1.8.7-ntest do gem env
+rvm 1.8.7-p374-ntest do gem env
 
-rvm 1.8.7-ntest do gem list
+rvm 1.8.7-p374-ntest do gem list
 # match[stderr]=/\A\Z/
 # match[stdout]!=/bundler/
 # match[stdout]!=/rake/
 # match[stdout]!=/rubygems-bundler/
 # match[stdout]!=/rvm/
 
-rvm 1.8.7-ntest do ruby -v
+rvm 1.8.7-p374-ntest do ruby -v
 # match=/1.8.7/
 
-rvm remove  1.8.7-ntest --gems
+rvm remove  1.8.7-p374-ntest --gems
 # status=0; match=/[Rr]emoving/
 
 
@@ -40,7 +40,7 @@ mkdir -p $rvm_path/gemsets/ruby/1.8.7/
 printf "gem-wrappers\nhaml\n" > $rvm_path/gemsets/ruby/1.8.7/default.gems
 printf "gem-wrappers\ntf\n"   > $rvm_path/gemsets/ruby/1.8.7/global.gems
 
-rvm install 1.8.7-ntest
+rvm install 1.8.7-p374-ntest
 # status=0
 # match!=/Already installed/
 # match=/[Aa]pplying patch/
@@ -48,24 +48,24 @@ rvm install 1.8.7-ntest
 # match=/WARNING: Please be aware that you just installed a ruby that/
 # match=/for a list of maintained rubies visit:/
 
-rvm 1.8.7-ntest do gem list
+rvm 1.8.7-p374-ntest do gem list
 # match[stderr]=/\A\Z/
 # match[stdout]=/haml/
 # match[stdout]=/tf/
 
-rvm 1.8.7-ntest@global do gem list
+rvm 1.8.7-p374-ntest@global do gem list
 # match[stderr]=/\A\Z/
 # match[stdout]!=/haml/
 # match[stdout]=/tf/
 
 ## Cleanup
 
-rvm remove 1.8.7-ntest --gems
+rvm remove 1.8.7-p374-ntest --gems
 # status=0; match=/[Rr]emoving/
 
 rm -rf $rvm_path/gemsets/ruby/1.8.7/
 
-find -L $rvm_path/environments -regextype posix-extended -regex '.*1.8.7-p[[:digit:]]+-ntest.*' -print -quit # match=/^$/
-find -L $rvm_path/wrappers     -regextype posix-extended -regex '.*1.8.7-p[[:digit:]]+-ntest.*' -print -quit # match=/^$/
-find -L $rvm_path/gems         -regextype posix-extended -regex '.*1.8.7-p[[:digit:]]+-ntest.*' -print -quit # match=/^$/
-find -L $rvm_path/bin          -regextype posix-extended -regex '.*1.8.7-p[[:digit:]]+-ntest.*' -print -quit # match=/^$/
+find -L $rvm_path/environments -name '*1.8.7-p374-ntest*' -print -quit # match=/^$/
+find -L $rvm_path/wrappers     -name '*1.8.7-p374-ntest*' -print -quit # match=/^$/
+find -L $rvm_path/gems         -name '*1.8.7-p374-ntest*' -print -quit # match=/^$/
+find -L $rvm_path/bin          -name '*1.8.7-p374-ntest*' -print -quit # match=/^$/
