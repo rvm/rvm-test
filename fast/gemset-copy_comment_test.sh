0ff64de06162cbf54840a2cfa937872321de09b8
Michal Papis <mpapis@gmail.com>
2014-05-28 05:16:07 +0200

    moving test away from just 1.8.7, update wayneeseguin/rvm#2869

--- fast/gemset-copy_comment_test.sh
+++ fast/gemset-copy_comment_test.sh
@@ -1,9 +1,9 @@
 source "$rvm_path/scripts/rvm"
 
-rvm use 1.8.7 --install
+rvm use 1.8.7-p374 --install
 
 :
-rvm gemset copy 1.8.7 1.8.7@testset # status=0; match=/Copying gemset/; match[stderr]=/^$/
+rvm gemset copy 1.8.7-p374 1.8.7-p374@testset # status=0; match=/Copying gemset/; match[stderr]=/^$/
 rvm gemset list                     # status=0; match=/ testset$/
 rvm gemset --force delete testset   # status=0; match=/Removing gemset testset/
 rvm gemset list                     # status=0; match!=/ testset$/
