7ded4b1cc19fda64e3c8fcf0e9d2ae99fc8774b8
Michal Papis <mpapis@gmail.com>
2014-05-28 05:11:29 +0200

    force use of -p374 for 1.8.7 to speed up tests, update wayneeseguin/rvm#2869

--- fast/current_and_list_comment_test.sh
+++ fast/current_and_list_comment_test.sh
@@ -1,11 +1,11 @@
 source "$rvm_path/scripts/rvm"
 
-rvm try_install 1.8.7
+rvm try_install 1.8.7-p374
 rvm try_install 1.9.3
 rvm try_install 2.0.0
 
 : separate default/current
-rvm use 1.8.7@abc-test --create --default
+rvm use 1.8.7-p374@abc-test --create --default
 # status=0
 # match=/Using .*ruby-1.8.7.* with gemset abc-test/
 rvm current
@@ -18,7 +18,7 @@ rvm current
 # match=/ruby-1.9.3.*@abc-test/
 
 rvm list
-# match=/^ \* ruby-1.8.7/
+# match=/^ \* ruby-1.8.7-p374/
 # match=/^=> ruby-1.9.3/
 
 : default == current
