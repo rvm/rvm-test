ead5abd8df55670e1262d4420747ffb44a446283
Benoit Daloze <eregontp@gmail.com>
2018-11-25 15:18:19 +0100

    Simplify test to use typical commands instead of -ntest

--- long/truffleruby_comment_test.sh
+++ long/truffleruby_comment_test.sh
@@ -1,6 +1,6 @@
-rvm reinstall truffleruby-ntest         # status=0; match!=/Already installed/; match=/compiling c-extensions/
-rvm truffleruby-ntest do ruby -v        # status=0; match=/truffleruby/
-rvm truffleruby-ntest do rake --version # status=0; match=/rake, version/
-rvm truffleruby-ntest do ruby -ropen-uri -e 'puts open("https://rubygems.org/") { |f| f.read(1024) }'
+rvm install truffleruby # status=0; match!=/Already installed/; match=/compiling c-extensions/
+rvm truffleruby do ruby -v # status=0; match=/truffleruby/
+rvm truffleruby do rake --version # status=0; match=/rake, version/
+rvm truffleruby do ruby -ropen-uri -e 'puts open("https://rubygems.org/") { |f| f.read(1024) }'
 # status=0; match=/RubyGems.org/
-rvm remove truffleruby-ntest            # status=0; match=/Removing/
+rvm remove truffleruby # status=0; match=/removing.+truffleruby/
